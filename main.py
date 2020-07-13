166b90fe2ff27c1ce6f3e840868a85e1e5e63db6
Ilya Semiletov <lookslike8@outlook.com>
2020-07-13 15:42:12 +0400

    0.2.13.07.0a added custom postfix option

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import fnmatch
 import getopt
 import sys
 
+
 class Resizer:
     def __init__(self):
         self.__errors_list = {"error_1": "The path to the file / folder is specified incorrectly.",
@@ -14,6 +15,7 @@ class Resizer:
         self.__images_folders_paths = []
         self.__images_objects_paths = []
         self.__final_resolution = None
+        self.__postfix = None
         self.__acceptable_file_extensions = "jpeg|jpg|png|txt|gif|bmp|tiff|exif|bat|webp"
 
         # Input parameters flags
@@ -21,12 +23,13 @@ class Resizer:
 
     def __console(self):
         self.__fm = False
+        self.__postfix = None
         if len(sys.argv) == 1:
             input_string = input(": ")
         else:
             separator = " "
             input_string = separator.join(sys.argv[1:])
-        #input_string = "r://test1.txt      r://test2.txt  -ir 100 200" # debug
+        #input_string = "r://test1.txt      r://test2.txt  -r 100 200" # debug
 
         # try to get exit/quit key
         if re.search(r"(?:^| |\n|-)(?:exit|quit)(?:|\n|$)", input_string, re.IGNORECASE):
@@ -37,17 +40,27 @@ class Resizer:
             self.__print_help()
             return 1
 
-        # get -ir key from input
-        ir_pattern = r"-ir \d+ ?\D ?\d+"
+        # get -r (resolution) key from input
+        ir_pattern = r"-r \d+ ?\D ?\d+"
         ir_data = re.findall(ir_pattern, input_string)
         if not ir_data:
             print(self.__errors_list["error_2"])
             return -2
-        # get -ir values
+        # get -r values
         self.__final_resolution = tuple(map(int, re.findall(r"\d+", ir_data[0], re.IGNORECASE)))
 
-        # delete -ir key from input_string
+        # delete -r key from input_string
         input_string = re.sub(ir_pattern, "", input_string, re.IGNORECASE)
+
+        # get -p (postfix) key from input
+        p_pattern = r"-p ?.*(?:$| )"
+        p_data = re.findall(p_pattern, input_string)
+        # get -p value if it exists
+        if p_data:
+            self.__postfix = p_data[0][2:]
+            self.__postfix = self.__postfix.strip()
+            # delete -p key from input_string
+            input_string = re.sub(p_pattern, "", input_string, re.IGNORECASE)
 
         pattern = r"(?:[a-zA-Z]:[\\\/])?(?:[\\\/]?[\w -]+)*(?:\.(?:{0}))?(?:\n| |$|\Z|\|)"
         pattern = pattern.format(self.__acceptable_file_extensions)
@@ -78,7 +91,7 @@ class Resizer:
             self.__resize_folders_images(x, self.__final_resolution)
 
         # debug
-        input()
+        # input()
 
     def start_console_listen(self, infinite=True):
         while True:
@@ -93,9 +106,16 @@ class Resizer:
         pattern = r".(?:{0})$"
         pattern = pattern.format(self.__acceptable_file_extensions)
         image_extension = re.search(pattern, image_path, re.IGNORECASE).group(0)
-        new_image_path = re.sub(pattern,
-        "_" + str(self.__final_resolution[0]) + "x" + str(self.__final_resolution[1])
-                                + image_extension, image_path, re.IGNORECASE)
+
+        new_pathname_end = ""
+        if self.__postfix:
+            new_pathname_end = self.__postfix + image_extension
+        else:
+            new_pathname_end = "_" + str(self.__final_resolution[0]) + \
+                               "x" + str(self.__final_resolution[1]) + \
+                               image_extension
+
+        new_image_path = re.sub(pattern, new_pathname_end, image_path, re.IGNORECASE)
 
         if self.__fm is not False:
             image.save(new_image_path, image_extension[1:])
@@ -115,18 +135,20 @@ class Resizer:
             self.__resize_images(image, self.__final_resolution)
         return 0
 
+    @staticmethod
     def __print_help(self):
-        print("""Use: path(one or more) [-ir [dec] [dec]] 
+        print("""Use: path(one or more) [-r [dec] [dec]] 
         Input files or folders with files one-by-one with space or comma separate. 
         use 'help' key to open this reference.
         use 'quit' or 'exit' ket to close this program.
-        use '-ir' key for setting new resolution.
+        use '-r' key for setting new resolution.
+        use '-k' key to set postfix for resized images.
         """)
 
 
 # main code part
-resizer = Resizer()
+application = Resizer()
 if len(sys.argv) == 1:
-    resizer.start_console_listen()
+    application.start_console_listen()
 else:
-    resizer.start_console_listen(False)
+    application.start_console_listen(False)
